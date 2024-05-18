e117276bc50975ee0ac691c14f4986a2f5e0c60f
Dize dev <axiozweb2022@gmail.com>
2024-05-17 21:49:04 -0500

    se optimizo el codigo para usar async await

--- js/app.js
+++ js/app.js
@@ -5,39 +5,40 @@ const lightbox = document.querySelector(".lightbox");
 const downloadImgBtn = lightbox.querySelector(".uil-import");
 const closeImgBtn = lightbox.querySelector(".close-icon");
 
-// API key, paginations, searchTerm variables
-const apiKey = "PASTE-YOUR-API-KEY";
+const apiKey = "jg9pkDr4VcyOsiQbMKNDKLFbRbrvJH3OG4NRFyqwxlCWdV2P8Guw4B9y";
 const perPage = 15;
 let currentPage = 1;
 let searchTerm = null;
 
-const downloadImg = (imgUrl) => {
-    // Converting received img to blob, creating its download link, & downloading it
-    fetch(imgUrl).then(res => res.blob()).then(blob => {
+const downloadImg = async (imgUrl) => {
+    try {
+        const res = await fetch(imgUrl);
+        const blob = await res.blob();
         const a = document.createElement("a");
         a.href = URL.createObjectURL(blob);
         a.download = new Date().getTime();
         a.click();
-    }).catch(() => alert("Failed to download image!"));
-}
+    } catch {
+        alert("Failed to download image!");
+    };
+};
 
 const showLightbox = (name, img) => {
-    // Showing lightbox and setting img source, name and button attribute
+
     lightbox.querySelector("img").src = img;
     lightbox.querySelector("span").innerText = name;
     downloadImgBtn.setAttribute("data-img", img);
     lightbox.classList.add("show");
     document.body.style.overflow = "hidden";
-}
+
+};
 
 const hideLightbox = () => {
-    // Hiding lightbox on close icon click
     lightbox.classList.remove("show");
     document.body.style.overflow = "auto";
 }
 
 const generateHTML = (images) => {
-    // Making li of all fetched images and adding them to the existing image wrapper
     imageWrapper.innerHTML += images.map(img =>
         `<li class="card">
             <img onclick="showLightbox('${img.photographer}', '${img.src.large2x}')" src="${img.src.large2x}" alt="img">
@@ -52,34 +53,37 @@ const generateHTML = (images) => {
             </div>
         </li>`
     ).join("");
-}
+};
 
-const getImages = (apiURL) => {
-    // Fetching images by API call with authorization header
+const getImages = async (apiURL) => {
     searchInput.blur();
     loadMoreBtn.innerText = "Loading...";
     loadMoreBtn.classList.add("disabled");
-    fetch(apiURL, {
-        headers: { Authorization: apiKey }
-    }).then(res => res.json()).then(data => {
+
+    try {
+        const res = await fetch(apiURL, {
+            headers: {
+                Authorization: apiKey
+            }
+        });
+        const data = await res.json();
         generateHTML(data.photos);
         loadMoreBtn.innerText = "Load More";
         loadMoreBtn.classList.remove("disabled");
-    }).catch(() => alert("Failed to load images!"));
-}
+    } catch {
+        alert("Failed to load images!");
+    };
+};
 
 const loadMoreImages = () => {
-    currentPage++; // Increment currentPage by 1
-    // If searchTerm has some value then call API with search term else call default API
+    currentPage++;
     let apiUrl = `https://api.pexels.com/v1/curated?page=${currentPage}&per_page=${perPage}`;
     apiUrl = searchTerm ? `https://api.pexels.com/v1/search?query=${searchTerm}&page=${currentPage}&per_page=${perPage}` : apiUrl;
     getImages(apiUrl);
 }
 
 const loadSearchImages = (e) => {
-    // If the search input is empty, set the search term to null and return from here
     if (e.target.value === "") return searchTerm = null;
-    // If pressed key is Enter, update the current page, search term & call the getImages
     if (e.key === "Enter") {
         currentPage = 1;
         searchTerm = e.target.value;
